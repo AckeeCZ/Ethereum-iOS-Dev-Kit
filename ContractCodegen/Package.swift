4b2b1522bd66f34c9563d161d192a04d78ef3473
Marek Fořt <marekfort@me.com>
2018-08-21 15:52:47 +0200

    Added PathKit.

--- ContractCodegen/Package.swift
+++ ContractCodegen/Package.swift
@@ -12,18 +12,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/jakeheis/SwiftCLI", .upToNextMinor(from: "5.2.0")),
+        .package(url: "https://github.com/kylef/PathKit", .upToNextMinor(from: "0.9.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ContractCodegenFramework",
-            dependencies: []),
+        .target(name: "ContractCodegenFramework"),
         .target(
             name: "contractgen",
             dependencies: [
                 "SwiftCLI",
-                .target(name: "ContractCodegenFramework")
+                "PathKit",
+                .target(name: "ContractCodegenFramework"),
             ]),
     ]
 )
